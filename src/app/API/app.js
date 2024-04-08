0b927930f22e8b62c8af5931f856292854e8364f
Dielder <dieldergabrieleal@hotmail.com>
2024-04-07 22:15:34 -0300

    Concertando erro de Commit

---
+++ src/app/API/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('API funcionando corretamente');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
