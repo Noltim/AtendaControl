eef7f4eb55432dae37e609edeb2bb89b1efd872b
Milton Oliveira <jose.milton@mv.com.br>
2024-04-09 22:59:35 -0300

    Fila para guardar senhas

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -11,24 +11,40 @@ export class SenhasService {
   public senhasTotal: number = this.senhasGeral + this.senhasPrior + this.senhasExame;
   public ultSenha: string = 'G';
 
+  public filaSenhas: string[] = [];
+  //Antes de gerar senha verifiar horário se está dentor do padrão das 7 às 17h caso n esteja n gerar senha
+  // Criar metodo para pegar posição final até a 5 ante final recebendo parametro de 0(final da fila) a 4(quinto elemento antes do final da fila)
+  // Ao  chamar no guiche usar a seguinte logica : [SP] -> [SE|SG] -> [SP] -> [SE|SG]
+  // Alterar padrão de nenha de N--- para : YYMMDD-PPSQ
+  //Criar fila juntando atendimento, guichê e status do atendimento.
+  //Status do guiche: Em atendimento ou vazio.
+  //Criar logica para finalizar atendimento enviar para fila com status: atendido ou não compareceu
+  //Criar relatorio com base nas filas criadas
+
   constructor() {
   }
 
   gerarSenhaGeral() {
     this.somaGeral();
     this.ultSenha = 'G';
+    const senha = this.ultSenha + this.senhasGeral.toLocaleString('pt-br', { minimumIntegerDigits: 3 });
+    this.filaSenhas.push(senha);
     this.salvarSenhaNoLocalStorage('senhaGeral', this.senhasGeral, this.ultSenha);
   }
 
   gerarSenhaPrior() {
     this.somaPrior();
     this.ultSenha = 'P';
+    const senha = this.ultSenha + this.senhasPrior.toLocaleString('pt-br', { minimumIntegerDigits: 3 });
+    this.filaSenhas.push(senha);
     this.salvarSenhaNoLocalStorage('senhaPrioritaria', this.senhasPrior, this.ultSenha);
   }
 
   gerarSenhaExame() {
     this.somaExame();
     this.ultSenha = 'E';
+    const senha = this.ultSenha + this.senhasExame.toLocaleString('pt-br', { minimumIntegerDigits: 3 });
+    this.filaSenhas.push(senha);
     this.salvarSenhaNoLocalStorage('senhaExame', this.senhasExame, this.ultSenha);
   }
 
