e591914e3c814d7784a3fcf5c783d0d849ee6ba6
Jailton Júnior DEV <jailtonjr99@hotmail.com.br>
2024-04-10 16:47:45 -0300

    VerificaçÃão de Horarios - FEITO

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -12,49 +12,66 @@ export class SenhasService {
   public ultSenha: string = 'G';
 
   public filaSenhas: string[] = [];
-  //Antes de gerar senha verifiar horário se está dentor do padrão das 7 às 17h caso n esteja n gerar senha
-  // Criar metodo para pegar posição final até a 5 ante final recebendo parametro de 0(final da fila) a 4(quinto elemento antes do final da fila)
-  // Ao  chamar no guiche usar a seguinte logica : [SP] -> [SE|SG] -> [SP] -> [SE|SG]
-  // Alterar padrão de nenha de N--- para : YYMMDD-PPSQ
-  //Criar fila juntando atendimento, guichê e status do atendimento.
-  //Status do guiche: Em atendimento ou vazio.
-  //Criar logica para finalizar atendimento enviar para fila com status: atendido ou não compareceu
-  //Criar relatorio com base nas filas criadas
+  public ultimasSenhasAtendidas: string[] = [];
+  // TODO Antes de gerar senha verifiar horário se está dentor do padrão das 7 às 17h caso n esteja n gerar senha - DONE
+  // TODO Criar metodo para pegar posição final até a 5 ante final recebendo parametro de 0(final da fila) a 4(quinto elemento antes do final da fila)
+  // TODO Ao  chamar no guiche usar a seguinte logica : [SP] -> [SE|SG] -> [SP] -> [SE|SG]
+  // TODO Alterar padrão de nenha de N--- para : YYMMDD-PPSQ
+  // TODO Criar fila juntando atendimento, guichê e status do atendimento.
+  // TODO Status do guiche: Em atendimento ou vazio.
+  // TODO Criar logica para finalizar atendimento enviar para fila com status: atendido ou não compareceu
+  // TODO Criar relatorio com base nas filas criadas
 
   constructor() {
   }
 
+  verificarHorario(): boolean {
+    return (new Date().getHours() >= 7 && (new Date().getHours() <= 16 && new Date().getMinutes() <= 45));
+  }
+
   gerarSenhaGeral() {
-    this.somaGeral();
-    this.ultSenha = 'G';
-    const senha = this.ultSenha + this.senhasGeral.toLocaleString('pt-br', { minimumIntegerDigits: 3 });
-    this.filaSenhas.push(senha);
-    this.salvarSenhaNoLocalStorage('senhaGeral', this.senhasGeral, this.ultSenha);
+    if (this.verificarHorario()) {
+      this.somaGeral();
+      this.ultSenha = 'G';
+      const senha = this.ultSenha + this.senhasGeral.toLocaleString('pt-br', {minimumIntegerDigits: 3});
+      this.filaSenhas.push(senha);
+      this.salvarSenhaNoLocalStorage('senhaGeral', this.senhasGeral, this.ultSenha);
+    } else {
+      alert("As senhas só podem ser geradas das 07:00 até às 16:45");
+    }
   }
 
   gerarSenhaPrior() {
-    this.somaPrior();
-    this.ultSenha = 'P';
-    const senha = this.ultSenha + this.senhasPrior.toLocaleString('pt-br', { minimumIntegerDigits: 3 });
-    this.filaSenhas.push(senha);
-    this.salvarSenhaNoLocalStorage('senhaPrioritaria', this.senhasPrior, this.ultSenha);
+    if (this.verificarHorario()) {
+      this.somaPrior();
+      this.ultSenha = 'P';
+      const senha = this.ultSenha + this.senhasPrior.toLocaleString('pt-br', {minimumIntegerDigits: 3});
+      this.filaSenhas.push(senha);
+      this.salvarSenhaNoLocalStorage('senhaPrioritaria', this.senhasPrior, this.ultSenha);
+    } else {
+      alert("As senhas só podem ser geradas das 07:00 até às 16:45");
+    }
   }
 
   gerarSenhaExame() {
-    this.somaExame();
-    this.ultSenha = 'E';
-    const senha = this.ultSenha + this.senhasExame.toLocaleString('pt-br', { minimumIntegerDigits: 3 });
-    this.filaSenhas.push(senha);
-    this.salvarSenhaNoLocalStorage('senhaExame', this.senhasExame, this.ultSenha);
+    if (this.verificarHorario()) {
+      this.somaExame();
+      this.ultSenha = 'E';
+      const senha = this.ultSenha + this.senhasExame.toLocaleString('pt-br', {minimumIntegerDigits: 3});
+      this.filaSenhas.push(senha);
+      this.salvarSenhaNoLocalStorage('senhaExame', this.senhasExame, this.ultSenha);
+    } else {
+      alert("As senhas só podem ser geradas das 07:00 até às 16:45");
+    }
   }
 
   mostrarSenha() {
     if (this.ultSenha === 'G') {
-      return 'G' + this.senhasGeral.toLocaleString('pt-br', { minimumIntegerDigits: 3 });
+      return 'G' + this.senhasGeral.toLocaleString('pt-br', {minimumIntegerDigits: 3});
     } else if (this.ultSenha === 'P') {
-      return 'P' + this.senhasPrior.toLocaleString('pt-br', { minimumIntegerDigits: 3 });
+      return 'P' + this.senhasPrior.toLocaleString('pt-br', {minimumIntegerDigits: 3});
     } else {
-      return 'E' + this.senhasExame.toLocaleString('pt-br', { minimumIntegerDigits: 3 });
+      return 'E' + this.senhasExame.toLocaleString('pt-br', {minimumIntegerDigits: 3});
     }
   }
 
@@ -75,6 +92,6 @@ export class SenhasService {
 
   private salvarSenhaNoLocalStorage(chave: string, valor: number, ultSenha: string) {
     localStorage.setItem(chave, valor.toString())
-    localStorage.setItem('ultimaChamada', ultSenha + valor.toLocaleString('pt-br', { minimumIntegerDigits: 3 }))
+    localStorage.setItem('ultimaChamada', ultSenha + valor.toLocaleString('pt-br', {minimumIntegerDigits: 3}))
   }
 }
